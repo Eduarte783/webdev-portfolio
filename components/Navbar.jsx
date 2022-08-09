c35bf5c60e7eeddcd952ca056081ff4ac2b7fbf5
Eduarte783 <eric.duarte83@gmail.com>
2022-08-09 14:07:56 -0700

    icon update4

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { useRouter } from "next/router";
-import navLogo from "../public/images/skills/navLogo.png"
+import navIcon from "../public/images/skills/navLogo.png"
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
@@ -55,7 +55,7 @@ const Navbar = () => {
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
 				<Link href="/">
 					<Image
-						src={navLogo}
+						src={navIcon}
 						alt="/"
 						width="105"
 						height="105"
@@ -104,7 +104,7 @@ const Navbar = () => {
 						<div className="flex w-full items-center justify-between">
 							<Link href="/">
 								<Image
-									src={navLogo}
+									src={navIcon}
 									width="80"
 									height="80"
 									alt="/"
